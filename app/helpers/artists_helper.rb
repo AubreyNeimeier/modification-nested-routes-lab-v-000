f8374998380245946efee1f0fe14c2837b47259c
Aubrey Neimeier <neimeier.1@osu.edu>
2018-05-29 18:07:00 +0000

    automatically backed up by learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -3,11 +3,11 @@ module ArtistsHelper
     song.artist.nil? ? link_to("Add Artist", edit_song_path(song)) : link_to(song.artist_name, artist_path(song.artist))
   end
 
-  def artist_select(@song)
+  def artist_select(song)
     if params[:artist_id].nil?
-      link_to "Add Artist", edit_artist_song_path(@song.artist, @song) if @song.artist
+      link_to "Add Artist", edit_artist_song_path(song.artist, song) if @song.artist
     else
-      @song.artist.name
+      song.artist.name
     end
   end
 
